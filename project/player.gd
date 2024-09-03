c9ffba446f84fb9acf19d3a656e894bbdb5e0d01
[William Robinson] <william.robinson@bsu.edu>
2024-09-03 09:38:22 -0400

    Add jump animation

--- project/player.gd
+++ project/player.gd
@@ -7,28 +7,31 @@ const JUMP_VELOCITY = -400.0
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		$AnimatedSprite.play("jump_end")
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		$AnimatedSprite.play("jump_start")
 		
-
 	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
-	if velocity.x != 0 and is_on_floor():
+		
+	if Input.is_action_pressed("jump"):
+		$AnimatedSprite.play("jump_start")
+	elif Input.is_action_pressed("left"):
 		$AnimatedSprite.play("run")
-	elif is_on_floor():
+		$AnimatedSprite.flip_h = true
+	elif Input.is_action_pressed("right"):
+		$AnimatedSprite.play("run")
+		$AnimatedSprite.flip_h = false
+	else:
 		$AnimatedSprite.play("idle")
 	
-	if velocity.x < 0:
-		$AnimatedSprite.scale.x = -1
-	elif velocity.x > 0:
-		$AnimatedSprite.scale.x = 1
+	if velocity.y > 0:
+		$AnimatedSprite.play("jump_end")
+	
+	
 		
 		
 	
--- project/world.gd
+++ project/world.gd
@@ -6,6 +6,6 @@ func _ready() -> void:
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_pressed("quit"):
 		get_tree().quit()
